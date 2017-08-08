cbcdefee9d0e3475bd6e4a5509bb1aee477e371f
Finn Herzfeld <finn@finn.io>
2017-08-08 09:56:18 -0700

    Store version as an actual string

--- orgsms/version.py
+++ orgsms/version.py
@@ -3,6 +3,6 @@ import subprocess
 __version__ = "dev"
 
 try:
-    __version__ = subprocess.check_output(['git', 'describe', '--tags', '--always']).strip()
+    __version__ = str(subprocess.check_output(['git', 'describe', '--tags', '--always'])).strip()
 except subprocess.CalledProcessError:
     pass
